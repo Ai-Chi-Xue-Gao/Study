9846f2dbfa1c245666dfec99ff8396b670071288
Ai-Chi-Xue-Gao <147791174+Ai-Chi-Xue-Gao@users.noreply.github.com>
2023-10-14 14:53:51 +0800

    Update GameTest.java

--- Day_09/src/test1/GameTest.java
+++ Day_09/src/test1/GameTest.java
@@ -38,6 +38,7 @@ public class GameTest {
                 System.out.println("很遗憾你被淘汰了");
                 System.out.println("master很遗憾你被淘汰了");
                 System.out.println("hot-fix很遗憾你被淘汰了");
+                System.out.println("push test");
             }
         }
     }
